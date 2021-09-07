88b5b310c2f423800687222bebf1aca34f8ae117
BDQ <bmatiychak@gmail.com>
2021-09-07 13:16:10 +0300

    created js mixin for gift registry with modal popup

--- app/design/frontend/Magento/elogic-theme/web/js/itemOverride.js
+++ app/design/frontend/Magento/elogic-theme/web/js/itemOverride.js
@@ -1,112 +1,82 @@
+// removed unnecessary dependencies and variables (5,9)
+// added login call variable + created mixin (13,14)
+// removed not used functions throughout whole documents
+// created showModal, redirectUser functions (61, 65)
+
 define([
     'jquery',
-    'uiComponent',
-    'underscore',
     'Magento_Customer/js/customer-data',
-    'mage/translate'
-], function($, Component, _, customerData, $t) {
+    'mage/url'
+], function($, customerData, url) {
     'use strict';
 
-    var login = 'login';
+    const loginCall = url.build('customer/account/login'), // ajax call for login url
+        mixin = {
 
-    return Component.extend({
-        defaults: {
-            hasRegistries: false,
-            url: null,
-            registries: [],
-            selected: [],
-            loginUrl: null,
-            newRegistryUrl: null
-        },
+            initialize: function () {
+                this._super();
+                this.initRegistries();
+            },
 
-        initialize: function () {
-            this._super();
-            this.initRegistries();
-        },
+            initRegistries: function() {
+                var customerRegistries = customerData.get('gift-registry')();
 
-        initRegistries: function() {
-            var customerRegistries = customerData.get('gift-registry')();
+                this.isLoggedIn(customerRegistries.is_logged_in || false);
+                this.registries(customerRegistries.registries || []);
+                this.selected(customerRegistries.selected || []);
+                this.hasRegistries(this.registries().length > 0);
+            },
 
-            this.isLoggedIn(customerRegistries.is_logged_in || false);
-            this.registries(customerRegistries.registries || []);
-            this.selected(customerRegistries.selected || []);
-            this.hasRegistries(this.registries().length > 0);
-        },
+            getData: function() {
+                var data = $('#product_addtocart_form').serializeArray();
+                if (_.size(this.selected()) > 0) {
+                    data.push({name: 'registries', value: _.map(this.selected(), function(value) { return value; })});
+                }
 
-        initObservable: function() {
-            this._super()
-                .observe([
-                    'selected',
-                    'registries',
-                    'hasRegistries',
-                    'isLoggedIn'
-                ]);
+                return data;
+            },
 
-            return this;
-        },
+            addProduct: function() {
+                if (!$('#product_addtocart_form').valid()) {
+                    return false;
+                }
 
-        defineBehaviour: function(data, event) {
-            this.initRegistries();
+                $.ajax({
+                    url: this.url,
+                    method: 'POST',
+                    data: this.getData(),
+                    dataType: 'json',
+                    showLoader: true,
+                    success: function (response) {
+                        var giftr = $('[data-block="addtogiftr"]');
+                        giftr.find('[data-role="dropdownDialog"]').dropdownDialog("close");
+                        $('.giftr-dropdown').hide();
+                        if (response.status == this.login) {
+                            setLocation(response.message);
+                        }
+                    }
+                });
+            },
 
-            if (this.registries().length == 1) {
-                event.stopPropagation();
-                this.addProduct();
-            }
-        },
+            showModal:function() {
+                $('.giftr-list').modal('openModal'); 
+            },
 
-        getData: function() {
-            var data = $('#product_addtocart_form').serializeArray();
-            if (_.size(this.selected()) > 0) {
-                data.push({name: 'registries', value: _.map(this.selected(), function(value) { return value })});
-            }
-
-            return data;
-        },
-
-        addProduct: function() {
-            if (!$('#product_addtocart_form').valid()) {
-                return false;
-            }
-
-            $.ajax({
-                url: this.url,
-                method: 'POST',
-                data: this.getData(),
-                dataType: 'json',
-                showLoader: true,
-                success: function (response) {
-                    var giftr = $('[data-block="addtogiftr"]');
-                    giftr.find('[data-role="dropdownDialog"]').dropdownDialog("close");
-                    $('.giftr-dropdown').hide();
-                    if (response.status == this.login) {
-                        setLocation(response.message);
+            redirectUser: function () {
+                if (!this.isLoggedIn()) {
+                    window.location.href = loginCall;
+                } else {
+                    if(this.registries().length == 1){
+                        this.addProduct();
+                    } else {
+                        this.showModal();
                     }
                 }
-            });
-        },
+            },
+        };
 
-        /**
-         * Should message be shown in gift registry dropdown or not.
-         */
-        isMessageVisible: function() {
-            return !this.hasRegistries() || !this.isLoggedIn();
-        },
+    return function (target) {
+        return target.extend(mixin);
+    };
 
-        /**
-         * Retrieve message for gift registry dropdown.
-         */
-        getMessage: function() {
-            var message = '';
-
-            if (!this.isLoggedIn()) {
-                message = $t('Please') + ', <a href="' + this.loginUrl + '">'+ $t('log in') +
-                    '</a> ' + $t('before adding products to registry');
-            } else if (!this.hasRegistries()) {
-                message = $t('You have no Gift Registries yet.') +
-                    ' <a href="' + this.newRegistryUrl + '">' + $t('Create Gift Registry') + '</a>';
-            }
-
-            return message;
-        }
-    });
 });
